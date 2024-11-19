abc6b8777f727ee64577fe815c515df017247c1b
saadetsimsek <saadetsimsek13@gmail.com>
2024-11-19 14:28:08 +0300

    Initial Commit

---
+++ CVCreaters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CVCreaters
+//
+//  Created by Saadet Şimşek on 19/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CVCreatersTests/CVCreatersTests.swift
@@ -0,0 +1,17 @@
+//
+//  CVCreatersTests.swift
+//  CVCreatersTests
+//
+//  Created by Saadet Şimşek on 19/11/2024.
+//
+
+import Testing
+@testable import CVCreaters
+
+struct CVCreatersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
